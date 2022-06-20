d80aa32cc318c509056c2695c074828b3c3ef009
Iktan Strategies <antonio.sanchez@iktanst.com>
2022-06-19 22:32:10 -0500

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <header className="py-10 bg-amber-500">
         <div className="container mx-auto flex flex-col lg:flex-row justify-between items-center">
-            <h1 className="font-bold text-2xl text-amber-200 text-center">Sistema de Administracion{' '} <span className="text-white font-black">de Constancias</span>
+            <h1 className="font-bold text-2xl text-amber-200 text-center">Dashboard{' '} <span className="text-white font-black">Administrativo</span>
             </h1>
             <nav className="flex flex-col items-center lg:flex-row gap-4 mt-5 lg:mt-0">
                 <Link to="/admin" className="text-white text-sm uppercase font-bold">Constancias</Link>
